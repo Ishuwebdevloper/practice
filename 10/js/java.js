3585d938ec78b172c0d54bf03d481656792ad210
Ishuwebdevloper <ishu27573@gmail.com>
2024-05-13 00:15:38 +0530

    10 Day

---
+++ 10/js/java.js
@@ -0,0 +1,52 @@
+setInterval(()=>{
+
+
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
+
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    hh.style.strokeDashoffset = 510 - (510 * h) / 12;
+    // 12 hrs clock
+
+
+    mm.style.strokeDashoffset = 630 - (630 * m) / 60;
+    // 60 minutes
+
+    ss.style.strokeDashoffset = 760 - (760 * s) / 60;
+    // 60 second
+    
+
+
+    let sec_dot = document.querySelector('.sec_dot');
+    let min_dot = document.querySelector('.min_dot');
+    let hr_dot = document.querySelector('.hr_dot');
+
+    sec_dot.style.transform = `rotateZ(${s * 6}deg)`;
+    // 360 / 60 second
+    min_dot.style.transform = `rotateZ(${m * 6}deg)`;
+    // 360 / 60 minutes
+    hr_dot.style.transform = `rotateZ(${h * 30}deg)`;
+    // 360 / 12 hrs
+
+
+
+
+    let hr = document.getElementById('hr');
+    let mn = document.getElementById('mn');
+    let sc = document.getElementById('sc');
+
+    hr.style.transform = `rotateZ(${h * 30}deg)`;
+    mn.style.transform = `rotateZ(${m * 6}deg)`;
+    sc.style.transform = `rotateZ(${s * 6}deg)`;
+
+
+
+
+
+})
+
+
